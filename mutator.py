a24c69ddade5f6c7a053ed5d4b31be3d2fc5506c
Yunqi Hao <yunqi.hao.au@gmail.com>
2025-10-30 12:31:38 +1100

    Added additive mutation

--- mutator.py
+++ mutator.py
@@ -1,13 +1,31 @@
 import random
+import os
+
+def additive(part: str) -> str:
+    """
+    Additive mutation. Adds an random number of bytes to the string, including non
+    16 byte aligned values.
+    """
+    _part = part[:].encode()
+    num_extra = random.randint(1, 999)
+    _part = _part + os.urandom(num_extra)
+    return _part
 
 def bitflip_mutation(part: str, mutation_index: int) -> str:
     _part = part[:].encode()
-    byte_index = mutation_index // 8
-    bit_index = mutation_index % 8
-    _part[byte_index] =  _part[byte_index] ^ 1 << (7 - bit_index)
+    if mutation_index:
+        byte_index = mutation_index // 8
+        bit_index = mutation_index % 8
+        _part[byte_index] =  _part[byte_index] ^  (1 << (7 - bit_index))
+    else:
+        num_changes = len(part) * 0.5
+        for _ in range(num_changes):
+            byte_index = random.randint(0, len(part) - 1)
+            bit_index = random.randint(0, 7)
+            _part[byte_index] = _part[byte_index] & (1 << (7 - bit_index))
     return _part.decode()
 
-def byteflip_mutation(part: str, mutation_index: int) -> str:
+def byteflip_mutation(part: str, mutation_index: int = 0) -> str:
     """
     Applies a byte flip mutation to the string.
     PARAMS:
@@ -17,21 +35,26 @@ def byteflip_mutation(part: str, mutation_index: int) -> str:
     # TODO: this may cause errors as it is passed into a file. Sometimes
     """
     # make a shadow copy
-    _part = part[:].encode()
+    _part = bytearray(part[:].encode())
     if mutation_index:
         _part[mutation_index] = random.randint(0, 255)
     else:
-        num_change = len(part) * 0.5
+        num_change = int(len(part) * 0.67)
         for _ in range(num_change):
             _part[random.randint(0, len(part) - 1)] =  random.randint(0, 255)
            
-    return _part.decode() #TODO: consider returnign a byte object
+    return bytes(_part) #TODO: consider returning a byte object
 
 def increment_mutation(part: str, mutation_index: int) -> str:
 
     return str(mutation_index)
 
-mutation_strategies = [bitflip_mutation, byteflip_mutation, increment_mutation]
+mutation_strategies = [
+    bitflip_mutation, 
+    byteflip_mutation, 
+    increment_mutation,
+    additive
+]
 
 def mutate(parts: list[str], mutation_count: int) -> list[str]:
     """Mutates one of the strings in parts based on the current mutation_count
@@ -57,5 +80,10 @@ def mutate(parts: list[str], mutation_count: int) -> list[str]:
 
     return mutated_parts
 
-
+# for testing purposes only
+for i in range(100):
+    res = byteflip_mutation("hello")
+    print(res)
+    print(len(res))
+    
 
