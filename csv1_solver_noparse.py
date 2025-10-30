1c4b3de6bc2ba28cdb8c1bde948cc9bb28194933
Yunqi <yunqi.hao.au@gmail.com>
2025-10-31 01:41:03 +1100

    Updated csv1 fuzz script for more efficiency and reliability. Lessened chance of crashing due to insufficient memory

--- csv1_solver_noparse.py
+++ csv1_solver_noparse.py
@@ -1,15 +1,13 @@
 from pwn import *
 import random
 
-"""
-For now, we need to solve csv1 by being 
-completely random
-
-"""
 def extend(item):
     num_times = random.randint(0, 100)
-    item = item * num_times
-    return bytearray(item)
+    try:
+        item = item * num_times
+        return bytearray(item)
+    except:
+        return item
     
 """
 Function that adds some fun amounts of bytes
@@ -18,14 +16,10 @@ def additive(item):
     if len(item) < 2:
         return item
     idx = random.randint(0, len(item) - 1)
-    # random_bytes = os.urandom(random.randint(900, 999)) <-- this might cause null bytes to be written, crashing the program
-    random_bytes = ''.join(random.choices(string.ascii_uppercase + string.digits, k=990))
+    random_bytes = ''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(0, 999)))
     item[idx:idx] = random_bytes.encode()
-    # for _ in range(15):
     return item
 
-
-from pwn import *
 input_file = "./csv1.txt"
 corpus = []
 strats = [
@@ -37,16 +31,19 @@ with open(input_file, 'rb') as file:
     all_bytes = bytearray(file.read())
     corpus.append(all_bytes)
 
-    # 100 iterations
-    for _ in range(1000):
+    for _ in range(10000):
         strategy = random.choice(strats)
         mutate_target = random.choice(corpus)[:]
-        # print(mutate_target)
         res = strategy(mutate_target)
 
         corpus.append(res)
+
+        if len(corpus) > 10:
+            # del corpus[1:len(corpus)//2]  
+            corpus = corpus[:len(corpus)//2]
+            corpus.insert(0, all_bytes)
+
         p = subprocess.run(["./csv1"],
             input=res    
         )
         print("rc", p.returncode)
-
