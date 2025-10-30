512df9d0886c9183d8305009f4e5f696e92e4b79
Yunqi <yunqi.hao.au@gmail.com>
2025-10-31 01:38:21 +1100

    Updated json1 fuzz script for more efficiency

--- json1_solver_noparse.py
+++ json1_solver_noparse.py
@@ -5,7 +5,7 @@ def extend(item):
     num_times = random.randint(0, 100)
     try:
         items = item * num_times
-        return bytearray(item)
+        return bytearray(items)
     except MemoryError:
         return item
     
@@ -16,17 +16,15 @@ def additive(item):
     if len(item) < 2:
         return item
     idx = random.randint(0, len(item) - 1)
-    # idx=14
-    # random_bytes = os.urandom(random.randint(900, 999)) <-- this might cause null bytes to be written
     random_bytes = ''.join(random.choices(string.digits, k=random.randint(0, 999)))
     item[idx:idx] = random_bytes.encode()
-    # for _ in range(15):
     return item
 
 input_file = "./json1.txt"
 corpus = []
 strats = [
-    additive
+    additive,
+    extend
 ]
 with open(input_file, 'rb') as file:
     all_bytes = bytearray(file.read())
@@ -36,15 +34,15 @@ with open(input_file, 'rb') as file:
     for _ in range(1000000):
         strategy = random.choice(strats)
         mutate_target = random.choice(corpus)[:]
-        print(mutate_target)
         res = strategy(mutate_target)
-        # res = mutate_target
-        print(res.decode())
-        # continue
 
-        # corpus.append(res)
+        corpus.append(res)
+        if len(corpus) > 10:
+            corpus = corpus[:len(corpus)//2]
+            corpus.insert(0, all_bytes)
+            print(len(corpus))
+
         p = subprocess.run(["./json1"],
             input=res
         )
         print("rc", p.returncode)
-
