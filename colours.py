432864150d7681d91d07f2b5857b892a9715332f
laughablehammer <kushaagra.kesarwani@gmail.com>
2025-10-30 09:15:54 +0000

    Read example inputs call correct class depending on file Also added some cool colours to terminal

---
+++ colours.py
@@ -0,0 +1,11 @@
+class Colours:
+    RESET = '\033[0m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    WHITE = '\033[97m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
