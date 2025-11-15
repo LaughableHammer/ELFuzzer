5e8802b214333b1ad6dff30659c3ea3a4652c7fe
laughablehammer <kushaagra.kesarwani@gmail.com>
2025-11-15 11:36:49 +0000

    Added magic value mutator

--- mutators/common_mutators.py
+++ mutators/common_mutators.py
@@ -18,3 +18,24 @@ def additive(item: bytearray) -> bytearray:
     random_bytes = ''.join(random.choices(string.ascii_uppercase, k=random.randint(1, 990)))
     item[idx:idx] = random_bytes.encode()
     return item
+
+# Gynvael’s Magic Numbers (https://h0mbre.github.io/Fuzzing-Like-A-Caveman/)
+def random_char(item: bytearray) -> bytearray:
+    magic_numbers = [b'%s', b'-1', b'0xFF', b'0x00', b'0xFFFF', b'0x0000', b'0x80000000', b'0x40000000', b'0x7FFFFFFF']
+    value = random.choice(magic_numbers)
+
+    if not item or len(item) < len(value):
+        return bytearray(value)
+    
+    mode = random.choice(['replace', 'insert', 'splice'])
+
+    match mode:
+        case 'replace':
+            return bytearray(value)
+        case 'insert':
+            idx = random.randint(0, len(item) - 1)
+            return item[:idx] + bytearray(item) + item[idx:]
+        case 'splice':
+            start = random.randint(0, len(item) - len(value))
+            end = start + len(value)
+            return item[:start] + value + item[end:]
