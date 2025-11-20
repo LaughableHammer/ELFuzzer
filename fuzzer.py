c9dff2fe018906b44e274aa428a2a6c2107020d3
laughablehammer <kushaagra.kesarwani@gmail.com>
2025-11-20 10:28:31 +0000

    Add another binary + make timeout executable

--- fuzzer.py
+++ fuzzer.py
@@ -5,18 +5,18 @@ import time
 import globalVar
 
 # binaries are here
-# binaries = Path('binaries/')
+binaries = Path('binaries/')
 
 # for additional testing
-binaries = Path('created_binaries/')
-# binaries = [Path('binaries/passcode1'), Path('binaries/csv1'), Path('binaries/json1'), Path('binaries/csv2')]
+#binaries = Path('created_binaries/')
+#binaries = [Path('created_binaries/timeout'), Path('binaries/csv1'), Path('binaries/json1'), Path('binaries/csv2')]
 
 overall_start = time.time()
 
 results = []
 
 for binary in binaries.iterdir():
-# for binary in binaries:
+#for binary in binaries:
     globalVar.init() # Reset global values
     print(f"{Colours.UNDERLINE}Fuzzing binary: {binary.name}{Colours.RESET}")
     
